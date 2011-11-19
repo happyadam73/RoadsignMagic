990e0fa3b086c21d5a4d04c4b445768c85e21647
Adam Buckley <abuckley@Adams-Macbook-Pro.local>
2011-11-19 17:14:08 +0000

    Initial Commit

---
+++ Roadsign Magic/AWBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AWBAppDelegate.h
+//  Roadsign Magic
+//
+//  Created by Adam Buckley on 19/11/2011.
+//  Copyright (c) 2011 Callcredit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AWBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Roadsign Magic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Roadsign Magic
+//
+//  Created by Adam Buckley on 19/11/2011.
+//  Copyright (c) 2011 Callcredit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AWBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AWBAppDelegate class]));
+    }
+}
